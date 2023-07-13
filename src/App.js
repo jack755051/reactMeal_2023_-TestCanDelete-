1928f3b55fd7b3bd75028f6751e5d47e50c892c0
jack755051 <71447741+jack755051@users.noreply.github.com>
2023-07-14 07:45:59 +0800

    168_over

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Fragment } from "react";
 import Header from "../src/components/Layouts/Header";
+import Meals from "./components/Meal/Meal";
 
 function App() {
   return (
     <React.Fragment>
-      <Header></Header>
+      <Header>
+        <main>
+          <Meals></Meals>
+        </main>
+      </Header>
     </React.Fragment>
   );
 }
